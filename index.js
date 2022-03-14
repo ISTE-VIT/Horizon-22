f63654fc2acddf636b0da9abed44371b18b6fc5d
sahildotexe <sahilkaling@gmail.com>
2022-03-14 22:38:32 +0530

    project initialized

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+app.use(express.json({ extended: false }));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log(`Server listening on port 3000`);
+});
