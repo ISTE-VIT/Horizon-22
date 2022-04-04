e44f386467a5717d309d75ab41a22c6fb165524b
sahildotexe <sahilkaling@gmail.com>
2022-04-04 08:20:53 +0530

    start script

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server listening on port 3000`);
 });
