470f62ef1fb1e2f466e5dfe89b2fa1331d70c905
sahildotexe <sahilkaling@gmail.com>
2022-04-06 21:30:57 +0530

    added routes

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ app.get("/", (req, res) => {
   res.render("index", { core: coredata });
 });
 
+app.get("/codeasymptote", (req, res) => {
+  res.render("events", { core: coredata });
+});
+
+app.get("/mlbootcamp", (req, res) => {
+  res.render("events", { core: coredata });
+});
+
+app.get("/webdev", (req, res) => {
+  res.render("events", { core: coredata });
+});
+
+app.get("/electronix", (req, res) => {
+  res.render("events", { core: coredata });
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server listening on port 3000`);
 });
